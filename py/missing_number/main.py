0ba48e979e71fc9bfed19fe0c02399cff7980a23
Rishabh Shah <26rkshah@gmail.com>
2023-01-01 12:42:01 -0800

    make missing number solution in python

---
+++ py/missing_number/main.py
@@ -0,0 +1,2 @@
+print(str(next(iter(set(range(1, int(input()) + 1)).symmetric_difference([int(i) for i in input().split()])))))
+# Get set of numbers `[1, n]`` and `[1, n] \ {missing number}` and find the symmetric difference of the two.
